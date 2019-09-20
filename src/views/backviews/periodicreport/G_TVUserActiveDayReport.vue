979f350e119eedd2313886387a05300905bfddb9
chenxuejiao <850780517@qq.com>
2019-09-20 09:31:11 +0800

    cxj_9/20
    cxj_9/20

--- src/views/backviews/periodicreport/G_TVUserActiveDayReport.vue
+++ src/views/backviews/periodicreport/G_TVUserActiveDayReport.vue
@@ -5,10 +5,15 @@
       <el-row class="chart_body back_white">
         <el-col class="height_auto periodic_report_title" :span="24">G+TV用户活跃发展日报表</el-col>
       </el-row>
-      <el-row class="rigesteduserBody">
-        <el-col class="height_auto" :span="24">
-          <numberOfRegisteredUsers :fillinData="registeredUsers"></numberOfRegisteredUsers>
-        </el-col>
+      <el-row>
+        <el-row class="model_title">
+          <span class="title_border_left"></span>在册用户数
+        </el-row>
+        <el-row class="rigesteduserBody">
+          <el-col class="height_auto" :span="24">
+            <numberOfRegisteredUsers :fillinData="registeredUsers"></numberOfRegisteredUsers>
+          </el-col>
+        </el-row>
       </el-row>
     </el-row>
     <!-- 用户注册人数结束 -->
@@ -55,13 +60,20 @@
 import numberOfRegisteredUsers from "@/views/backcoms/G_TVUserActiveDayReport/numberOfRegisteredUsers"; //在册用户数
 import dayREportBar from "@/views/backcoms/G_TVUserActiveDayReport/dayREport_bar"; //用户数统计柱状组件
 import dayREportBarLv from "@/views/backcoms/G_TVUserActiveDayReport/dayREport_bar_lv"; //用户数统计柱状组件带百分比
+
+import { mapGetters } from "vuex";
+import { liveUsers_daliyReport } from "@/api/api_main";
+
 export default {
   name: "G_TVUserActiveDayReport", //G+TV用户活跃发展日报表
-  // created() {
-  //   this.liveUsers_daliyReport("yd");
-  //   this.liveUsers_daliyReport("lt");
-  //   this.liveUsers_daliyReport("dx");
-  // },
+  components: {
+    numberOfRegisteredUsers,
+    dayREportBar,
+    dayREportBarLv
+  },
+  computed: {
+    ...mapGetters(["PR_operator", "PR_day"])
+  },
   data() {
     return {
       //在册用户数
@@ -103,8 +115,8 @@ export default {
         id: "outLookTime_UADR",
         color: ["#82b65f", "#70a7d8"],
         data: [
-          [["运营商", "移动", "联通", "电信"], ["平均", 7.0, 8.5, 0]],
-          [["运营商", "移动", "联通", "电信"], ["今日", 6.4, 7.5, 0]]
+          [["运营商", "移动", "联通", "电信"], ["平均", 0, 0, 0]],
+          [["运营商", "移动", "联通", "电信"], ["今日", 0, 0, 0]]
         ]
       },
 
@@ -112,10 +124,10 @@ export default {
       dayLooktime: {
         title: "三大基础功能单日观看时长（万小时）",
         id: "dayLooktime_UADR",
-        color: ["#82b65f", "#70a7d8"],
+        color: ["#70a7d8"],
         data: [
-          [["运营商", "移动", "联通", "电信"], ["平均", 77.4, 143.8, 0]],
-          [["运营商", "移动", "联通", "电信"], ["今日", 78.7, 114.2, 0]]
+          // [["运营商", "移动", "联通", "电信"], ["平均", 77.4, 143.8, 0]], //清单中未提及均值，所以接口中没有得到数据
+          [["运营商", "直播", "点播", "回看"], ["今日", 78.7, 114.2, 0]]
         ]
       },
 
@@ -161,18 +173,99 @@ export default {
       }
     };
   },
-  components: {
-    numberOfRegisteredUsers,
-    dayREportBar,
-    dayREportBarLv
+  created() {
+    this.liveUsers_daliyReport("yd");
+    this.liveUsers_daliyReport("lt");
+    this.liveUsers_daliyReport("dx");
+  },
+  methods: {
+    //G+TV用户活跃发展日报表
+    liveUsers_daliyReport(type) {
+      let vm = this;
+      let ttt;
+      if (type == "yd") {
+        ttt = "移动";
+      }
+      if (type == "lt") {
+        ttt = "联通";
+      }
+      if (type == "dx") {
+        ttt = "电信";
+      }
+      // console.log("~~~~~~!!vm.PR_day");
+      // console.log(vm.PR_day);
+      let start = "2019-06-02";
+      let end = "2019-06-02";
+      if (vm.PR_day != null) {
+        start = vm.PR_day.start;
+        end = vm.PR_day.end;
+      }
+
+      let temp = {
+        operator: String([ttt]),
+        start: start,
+        end: start
+      };
+      var formData = new FormData();
+      var formData = new window.FormData();
+      formData.append("operator", temp.operator);
+      formData.append("start", temp.start);
+      formData.append("end", temp.end);
+
+      //户均观看时长api对接
+      liveUsers_daliyReport(formData)
+        .then((response) => {
+          console.log("G+TV用户活跃发展日报表", response.data)
+
+          //户均观看时长（今日）
+          let watch_freq_family = this.returnFloat(response.data.responses[0].aggregations.watch_freq_family.value)
+          // console.log(watch_freq_family)
+
+          if (type == "yd") {
+            // console.log("移动今日观看", this.outLookTime.data[1][1][1])
+            this.outLookTime.data[1][1][1] = watch_freq_family  //设置户均观看时长（今日维度）
+          }
+          if (type == "lt") {
+            // console.log("联通今日观看", this.outLookTime.data[1][1][2])
+            this.outLookTime.data[1][1][2] = watch_freq_family
+          }
+          if (type == "dx") {
+            // console.log("电信今日观看", this.outLookTime.data[1][1][3])
+            this.outLookTime.data[1][1][3] = watch_freq_family
+          }
+
+        })
+        .catch((error) => {
+          console.log("G+TV用户活跃发展日报表", error)
+        })
+
+      //三大基础功能单日观看时长api对接
+
+
+    },
+
+    //保留两位小数
+    returnFloat(value) {
+      // 保留两位小数
+      var value = Math.round(parseFloat(value) * 100) / 100;
+      var xsd = value.toString().split(".");
+      if (xsd.length == 1) {
+        value = value.toString() + ".00";
+        return value;
+      }
+      if (xsd.length > 1) {
+        if (xsd[1].length < 2) {
+          value = value.toString() + "0";
+        }
+        return value;
+      }
+    },
   }
-};
+}
 </script>
 <style scoped>
 .viewing_behavior_report {
-  /* height: 500px; */
   margin: 14px 0px;
-  /* border: 1px solid #777; */
 }
 .rigesteduserBody {
   height: 400px;
